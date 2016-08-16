a3489e89571a052856a339c2e4d5ff69d47cf8ec
Michael Malura <maluramichael@googlemail.com>
2016-08-16 16:56:54 +0200

    Initialize project

---
+++ index.js
@@ -0,0 +1,214 @@
+/**
+ * Created by maluramichael on 16/08/16.
+ */
+
+import React, {Component} from 'react';
+import {
+	View,
+	Text,
+	Image,
+	Alert,
+	Platform,
+	Navigator,
+	BackAndroid,
+	TouchableHighlight,
+	AsyncStorage
+} from 'react-native';
+import {
+	TextTitlebar,
+	BackButton,
+	MenuButton
+} from '../';
+import Style from './style.js';
+import {connect} from 'react-redux';
+
+export default class BetterNavigator extends Component {
+
+	/**********************************************************************
+	 * Component
+	 **********************************************************************/
+	constructor(props) {
+		super(props);
+
+		/**********************************************************************
+		 * Navigator
+		 **********************************************************************/
+		this.renderScene = this.renderScene.bind(this);
+		this.renderNavigationBar = this.renderNavigationBar.bind(this);
+		this.createRouteMapper = this.createRouteMapper.bind(this);
+		this.mapTitleToRoute = this.mapTitleToRoute.bind(this);
+		this.mapLeftButtonToRoute = this.mapLeftButtonToRoute.bind(this);
+		this.mapRightButtonToRoute = this.mapRightButtonToRoute.bind(this);
+		this.onRouteWillFocus = this.onRouteWillFocus.bind(this);
+		this.callRouteFunction = this.callRouteFunction.bind(this);
+
+		/**********************************************************************
+		 * Navigation bar
+		 **********************************************************************/
+		this.onPressMenuButton = this.onPressMenuButton.bind(this);
+		this.onPressBackButton = this.onPressBackButton.bind(this);
+		this.showNavigationBar = this.showNavigationBar.bind(this);
+		this.hideNavigationBar = this.hideNavigationBar.bind(this);
+		this.setNavigationBarColor = this.setNavigationBarColor.bind(this);
+
+		this.state = {
+			navigationBarColor : '#333',
+			navigationBarHidden: true
+		};
+
+		this.cachedRoutes = new Map();
+	}
+
+	render() {
+		const sceneStyle = this.state.navigationBarHidden ? {marginTop: null} : {};
+
+		return (
+
+			<Navigator
+				initialRoute={this.props.initialRoute}
+				renderScene={this.renderScene}
+				onWillFocus={this.onRouteWillFocus}
+				navigationBar={this.renderNavigationBar()}
+				sceneStyle={[Style.page, sceneStyle]}
+				ref={'navigator'}
+			/>
+		);
+	}
+
+	/**********************************************************************
+	 * Navigation bar
+	 **********************************************************************/
+	onPressMenuButton() {
+		this.openSideMenu()
+	}
+
+	onPressBackButton() {
+		if (this.refs.navigator.state.presentedIndex > 0) {
+			this.refs.navigator.pop();
+		}
+	}
+
+	showNavigationBar() {
+		this.setState({navigationBarHidden: false});
+	}
+
+	hideNavigationBar() {
+		this.setState({navigationBarHidden: true});
+	}
+
+	setNavigationBarColor(color) {
+		this.setState({navigationBarColor: color});
+	}
+
+	/**********************************************************************
+	 * Navigator
+	 **********************************************************************/
+	callRouteFunction(functionName, route, navigator, index, navState) {
+		const Route = this.props.routes(route);
+		if (Route && Route[functionName]) {
+			return Route[functionName](route, navigator, index, navState);
+		}
+		return null;
+	}
+
+	renderScene(route, navigator) {
+		if (this.cachedRoutes.has(route.name)) {
+			return this.cachedRoutes.get(route.name);
+		} else {
+			const Route = this.props.routes(route);
+			if (Route) {
+				const stateMapper = Route.mapState ? Route.mapState : ()=> {
+					return {Store: {}}
+				};
+				const actionMapper = Route.mapActions ? Route.mapActions : ()=> {
+					return {Actions: {}}
+				};
+
+				const connectedRoute = connect(stateMapper, actionMapper)(Route);
+				const element = React.createElement(connectedRoute, {...route.passProps, navigator: navigator});
+
+				this.cachedRoutes.set(route.name, element);
+
+				return element;
+			}
+
+			return <Text>404</Text>;
+		}
+	}
+
+	renderNavigationBar() {
+		if (this.state.navigationBarHidden) {
+			return <View/>;
+		}
+
+		const navigationBarStyle = {
+			backgroundColor: this.state.navigationBarColor
+		};
+
+		return (
+			<Navigator.NavigationBar
+				routeMapper={this.createRouteMapper()}
+				style={[Style.navigationBar, navigationBarStyle]}
+			/>
+		);
+	}
+
+	mapTitleToRoute(route, navigator, index, navState) {
+		const navigatorItem = this.callRouteFunction('getTitleBar', route, navigator, index, navState);
+		if (navigatorItem) {
+			return navigatorItem;
+		}
+
+		return (
+			<TextTitlebar title={route.title ? route.title.toUpperCase() : ''}
+						  textStyle={[Style.titleBarText, route.titleTextStyle]}
+						  containerStyle={[route.titleStyle]}
+			/>
+		);
+	}
+
+	mapLeftButtonToRoute(route, navigator, index, navState) {
+		const navigatorItem = this.callRouteFunction('getLeftButton', route, navigator, index, navState);
+		if (navigatorItem) {
+			return navigatorItem;
+		}
+
+		if (index > 0) {
+			return <BackButton onPress={this.onPressBackButton}/>;
+		} else if (index === 0) {
+			return <MenuButton onPress={this.onPressMenuButton}/>;
+		}
+
+		return <View/>;
+	}
+
+	mapRightButtonToRoute(route, navigator, index, navState) {
+		const navigatorItem = this.callRouteFunction('getRightButton', route, navigator, index, navState);
+		if (navigatorItem) {
+			return navigatorItem;
+		}
+
+		return <View/>;
+	}
+
+	createRouteMapper() {
+		return {
+			LeftButton : this.mapLeftButtonToRoute,
+			RightButton: this.mapRightButtonToRoute,
+			Title      : this.mapTitleToRoute,
+		}
+	}
+
+	onRouteWillFocus(nextRoute) {
+		const titleBarColor = this.callRouteFunction('getTitleBarColor', nextRoute);
+		const isTitleBarVisible = this.callRouteFunction('isTitleBarVisible', nextRoute);
+
+		if (typeof titleBarColor !== 'undefined' && titleBarColor !== null) {
+			this.setNavigationBarColor(titleBarColor);
+		}
+
+		if (typeof isTitleBarVisible !== 'undefined' && isTitleBarVisible !== null) {
+			isTitleBarVisible ? this.showNavigationBar() : this.hideNavigationBar();
+		}
+	}
+}
---
+++ style.js
@@ -0,0 +1,26 @@
+/**
+ * Created by michaelmalura on 30/07/16.
+ */
+
+import React, {Component} from 'react';
+import {StyleSheet} from 'react-native';
+import Constants from '../../core/constants';
+
+export default StyleSheet.create({
+	navigator    : {
+		flex           : 1,
+		backgroundColor: 'white'
+	},
+	navigationBar: {
+		backgroundColor: '#333'
+	},
+	titleBarText : {
+		color     : 'white',
+		fontWeight: 'bold'
+	},
+	page         : {
+		flex           : 1,
+		marginTop      : Constants.NavigationBarHeight,
+		backgroundColor: 'white'
+	}
+});
